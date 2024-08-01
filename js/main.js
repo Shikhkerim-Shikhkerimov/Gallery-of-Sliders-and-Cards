9aefc7266ebb96fec6fdd61e27595cabc5a4d901
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-08-01 15:28:44 +0400

    edited the first slide

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-let prev = document.querySelector(".prev")
-let next = document.querySelector(".next")
-let slider = document.querySelector(".slider")
+let prev = document.querySelector(".slider1__prev")
+let next = document.querySelector(".slider1__next")
+let slider = document.querySelector(".slider1__slider")
 
 prev.addEventListener("click", function() {
-    let slides = document.querySelectorAll(".slides")
+    let slides = document.querySelectorAll(".slider1__slides")
     slider.append(slides[0])
     console.log(slider);
 })
 
 next.addEventListener("click", function() {
-    let slides = document.querySelectorAll(".slides")
+    let slides = document.querySelectorAll(".slider1__slides")
     slider.prepend(slides[slides.length - 1])
     console.log(slider);
 })
