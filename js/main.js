59f80ff0e51db4aa46e45d69ce065e6ae7df9671
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-08-04 23:01:24 +0400

    edited the second slide

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ let slider = document.querySelector(".slider1__slider")
 prev.addEventListener("click", function() {
     let slides = document.querySelectorAll(".slider1__slides")
     slider.append(slides[0])
-    console.log(slider);
 })
 
 next.addEventListener("click", function() {
     let slides = document.querySelectorAll(".slider1__slides")
     slider.prepend(slides[slides.length - 1])
-    console.log(slider);
 })
 
-// слайдер 1
+// слайдер 2
+const slider2 = document.querySelectorAll(".slider2__slides")
+
+for (let value of slider2) {
+    value.addEventListener("click", function() {
+        document.querySelector(".slidesActive")?.classList.remove("slidesActive")
+        value.classList.toggle("slidesActive")
+    })
+}
